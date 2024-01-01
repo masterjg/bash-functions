06a30adb6720ca1f8fa6184895ef89184dacdba3
Marius Guščius <marius.guscius@mailbox.org>
2024-01-01 14:46:58 +0200

    path fix

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 
 functions_path="$(awk -F= '/^(ID|VERSION_ID|VARIANT_ID)="?/ { gsub(/"/, ""); v[++n]=$2 } END { print v[1] "/" v[3] "/" v[2] }' /etc/os-release)"
 
-source "functions/${functions_path}/__loader.sh"
+source "$(dirname "${BASH_SOURCE}")/functions/${functions_path}/__loader.sh"

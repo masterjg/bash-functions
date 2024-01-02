fda87d33b2e7ab6c3c58b24da78e13229b31d912
Marius Guščius <marius.guscius@mailbox.org>
2024-01-02 15:02:15 +0200

    clone f

---
+++ functions/fedora/kinoite/39/directory/clone.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Example use:
+# directory.clone /etc "${top_level_mount}/etc" -y
+
+local source="${1/#\~/${HOME}}"
+local destination="${2/#\~/${HOME}}"
+local sudo="${3:+sudo}"
+
+${sudo} rsync -aHAX --delete "${source}/" "${destination}"
+${sudo} chmod --reference="${source}" "${destination}"
+${sudo} chown --reference="${source}" "${destination}"
+${sudo} chcon --reference="${source}" "${destination}"
